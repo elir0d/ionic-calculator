4a580991be52f42b69e7e80401273575ae212aa7
elir0d <49011043+elir0d@users.noreply.github.com>
2021-10-07 01:17:17 -0300

    feat:

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,81 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  result: string;
+  operation: string;
+
+  currentNumber = '0';
+  firstOperand = null;
+  operator = null;
+  waitForSecondNumber = false;
+
+  constructor() {
+    this.operation = '188 x 8';
+    this.result = '1504';
+  }
+
+  //listening methods//
+  public getNumber(v: string){
+    console.log(v);
+    if(this.waitForSecondNumber)
+    {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    }else{
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+
+    }
+  }
+
+  getDecimal(){
+    if(!this.currentNumber.includes('.')){
+        this.currentNumber += '.';
+    }
+  }
+
+  //Calculation operation//
+  private doCalculation(op , secondOp){
+    switch (op){
+      case '+':
+      return this.firstOperand += secondOp;
+      case '-':
+      return this.firstOperand -= secondOp;
+      case '*':
+      return this.firstOperand *= secondOp;
+      case '/':
+      return this.firstOperand /= secondOp;
+      case '=':
+      return secondOp;
+    }
+  }
+
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  public getOperation(op: string){
+    console.log(op);
+
+    if(this.firstOperand === null){
+      this.firstOperand = Number(this.currentNumber);
+
+    }else if(this.operator){
+      const result = this.doCalculation(this.operator , Number(this.currentNumber));
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  public clear(){
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
 
 }
